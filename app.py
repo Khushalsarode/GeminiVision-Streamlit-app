723612233d112e2341eaa92766f727067ebe73f1
Khushalsarode <khushalsarode.in@gmail.com>
2025-04-01 13:07:41 +0530

    updated for lint

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
+"""
+# Streamlit app for interacting with Gemini Vision API
+# This app allows users to upload an image and ask questions about it.
+# The app uses the Google Gemini API to generate responses based on the image and user input.
+"""
 import os
+import time
 import logging
 import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as genai
 from PIL import Image
-import time
 
 #uncomment the following line to enable Streamlit's secrets management
 #.streamlit/secrets.toml
 # Access secrets
 #GEMINI_SECRET_KEY = st.secrets["secrets"]["api_key"]
 #print("key:"+ GEMINI_SECRET_KEY)
-
 
 #.env file for secrets management
 # Load environment variables from the .env file (if present)
@@ -22,7 +26,6 @@ GEMINI_SECRET_KEY = os.getenv('GOOGLE_GEMINI_AI_KEY')
 if GEMINI_SECRET_KEY:
     # Example usage
     print(f'SECRET_KEY: {GEMINI_SECRET_KEY}')
-
     # Configure Gemini API Key
     genai.configure(api_key=GEMINI_SECRET_KEY)
     print("Gemini API Key configured.")
@@ -41,7 +44,6 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
     logger.info("Session Cleared.")
 
-
 # Sidebar for instructions
 st.sidebar.title("Instructions")
 st.sidebar.write("1. Upload an image using the file uploader.")
@@ -53,6 +55,13 @@ st.sidebar.write("7. The chat history will be displayed below.")
 
 # Gemini Vision API Call
 def chat_with_gemini(image, text):
+    """Function to call Gemini Vision API with image and text prompt.
+    
+    Args:
+        image (PIL.Image): The uploaded image.
+        text (str): The text prompt.
+        
+    """
     model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content([image, text])
     logger.info("Gemini Vision API called successfully.")
@@ -63,7 +72,6 @@ def chat_with_gemini(image, text):
     # Return the text content of the response
     return response.text
 
-    
 # Upload Image
 uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if uploaded_file:
@@ -73,7 +81,6 @@ if uploaded_file:
 else:
     st.session_state.image = None
     logger.info("No image uploaded.")
-
 
 # User Prompt
 prompt = st.text_input("Enter your prompt:",key="user_input")
@@ -86,18 +93,13 @@ if st.button("Submit") and prompt:
         with st.spinner("Wait for it...", show_time=True):
             time.sleep(2)
             st.toast("Be Patience!", icon="⏳")
-        #st.badge("Success", icon="📝", color="blue")
         st.session_state.messages.append({"role": "user", "content": prompt})
-        logger.info(f"User prompt submitted")
-            
-            # Get AI response
-        ai_response = chat_with_gemini(st.session_state.image, prompt)
-        #st.badge("Success", icon="🤖", color="green")
-        st.session_state.messages.append({"role": "assistant", "content": ai_response})
-
-        logger.info(f"AI response received")
+        logger.info("User prompt submitted")
+        # Get AI response
+        AI_RESPONSE = chat_with_gemini(st.session_state.image, prompt)
+        st.session_state.messages.append({"role": "assistant", "content": AI_RESPONSE})
+        logger.info("AI response received")
         
-
 st.sidebar.write("### Clear Session!")
 if st.sidebar.button("Clear History"):
     st.session_state.image = None
@@ -105,11 +107,9 @@ if st.sidebar.button("Clear History"):
     logger.info("Session fully cleared.")
     st.toast("Chat history cleared!", icon="🧹")
     
-
 st.write("### Chat History")
 for msg in st.session_state.messages:
     col1, col2 = st.columns([1, 1], vertical_alignment="top")  # Two equal columns
-
     if msg["role"] == "user":
         with col1:  # Left side for user
             st.markdown("📝 **User**")
